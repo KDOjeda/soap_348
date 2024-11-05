7b58caaeb360c6fe5dcc4284de78e45c082c6a62
GedAmbay <mboyambay04@gmail.com>
2024-11-05 18:56:18 +0800

    ror

--- app/contact.js
+++ app/contact.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const contact = () => {
   return (
-    <div className='title'>Contact Us!</div>
+    <div className='title'>
+        Contact Us!
+        <h1>Nothing's gonna change my love for you!</h1>
+        </div>
   )
 }
